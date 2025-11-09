25bebf2538e26cb80b9de7cfe955957e0a2d662c
tyange <usun16@gmail.com>
2025-11-10 00:06:03 +0900

    added github link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import Editor from '@/app/components/editor'
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
       <Editor />
+      <div className="w-full flex justify-center items-center mt-3">
+        <Link href="https://github.com/tyange/canvas-playground">
+          <Image src="/github.svg" alt="Github icon" width={24} height={24} />
+        </Link>
+      </div>
     </div>
   )
 }
