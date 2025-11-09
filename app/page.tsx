c50ca0a37099381479c429c580cade2eedccc6d8
tyange <usun16@gmail.com>
2025-11-10 00:48:09 +0900

    added guide

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import Editor from '@/app/components/editor'
 export default function Home() {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <div className="mb-3">
+        <p>'Select File' 버튼으로 이미지를 선택하시고, 이미지 위를 드래그 해서 가려보세요.</p>
+      </div>
       <Editor />
       <div className="w-full flex justify-center items-center mt-3">
         <Link href="https://github.com/tyange/canvas-playground">
